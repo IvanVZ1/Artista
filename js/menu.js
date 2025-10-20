70d02fadd97290db53fff36e7058810c11809bf6
IvanVZ1 <ivan101110x@gmail.com>
2025-10-20 00:03:08 -0600

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+(function () {
+  const openButton = document.querySelector(".nav-menu");
+  const menu = document.querySelector(".nav-link-menu");
+  const closeButton = document.querySelector(".nav-close");
+
+  openButton.addEventListener("click", () => {
+    menu.classList.add("nav-link--show");
+  });
+
+  closeButton.addEventListener("click", () => {
+    menu.classList.remove("nav-link--show");
+  });
+})();
---
+++ js/music.js
@@ -0,0 +1,8 @@
+    // Reproductor JS básico
+    const audio = document.getElementById("audio");
+    const playBtn = document.getElementById("play");
+    const prevBtn = document.getElementById("prev");
+    const nextBtn = document.getElementById("next");
+    const progress = document.getElementById("progress");
+    const volume = document.getElementById("volume");
+    const songTitle = document.getElement
